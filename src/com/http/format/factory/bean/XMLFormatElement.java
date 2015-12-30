23c6440101e24590c1c03c15f8e9e9fe78c57acf
chenssy89 <chenssy995812509@163.com>
2015-12-30 15:49:18 +0800

    commit

---
+++ src/com/http/format/factory/bean/XMLFormatElement.java
@@ -0,0 +1,18 @@
+package com.http.format.factory.bean;
+
+/**
+ * 报文模板<br>
+ * 模板中主要包括两个元素：报文头head、报文标签tag
+ * @Project:JDMHA
+ * @file:XMLFormatElement.java
+ *
+ * @Author:chenssy
+ * @email:chenssy995812509@163.com
+ * @url : <a href="http://cmsblogs.com">http://cmsblogs.com</a>
+ * @qq : 122448894
+ *
+ * @data:2015年12月30日
+ */
+public class XMLFormatElement {
+	
+}
--- src/com/http/format/factory/config/XMLDefinition.java
+++ src/com/http/format/factory/config/XMLDefinition.java
@@ -1,7 +1,9 @@
 package com.http.format.factory.config;
 
+import com.http.format.factory.bean.XMLFormatElement;
+
 /**
- * xml格式文件的内部数据结构
+ * JDMHA 报文模板容器的内部数据结构，相当于 IoC容器的BeanDefinition
  * 
  * @Project:JDMHA
  * @file:XMLDefinition.java
@@ -14,5 +16,36 @@ package com.http.format.factory.config;
  * @data:2015年12月29日
  */
 public class XMLDefinition {
+	/** xml文件编号 */
+	private String xmlId;
+	
+	/** xml 发送报文模板 */
+	private XMLFormatElement sentFormatElement;
+	
+	/** xml 接收报文模板  */
+	private XMLFormatElement receiveFormatElement;
 
+	public String getXmlId() {
+		return xmlId;
+	}
+
+	public void setXmlId(String xmlId) {
+		this.xmlId = xmlId;
+	}
+
+	public XMLFormatElement getSentFormatElement() {
+		return sentFormatElement;
+	}
+
+	public void setSentFormatElement(XMLFormatElement sentFormatElement) {
+		this.sentFormatElement = sentFormatElement;
+	}
+
+	public XMLFormatElement getReceiveFormatElement() {
+		return receiveFormatElement;
+	}
+
+	public void setReceiveFormatElement(XMLFormatElement receiveFormatElement) {
+		this.receiveFormatElement = receiveFormatElement;
+	}
 }
